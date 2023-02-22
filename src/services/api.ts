167995aa058b24c9b07c9fa4ca7d4ff2579a35ab
Júlio Filho <juliorodriguesfilho@gmail.com>
2023-02-22 16:37:46 -0300

    feat: alterado param

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL: "http://179.124.195.83:6017/api/MILLENIUM!PRD.PRODUCAO.PRODUTOFASES",
 });
 
-api.defaults.headers.common["WTS-LicenceType"] = "retag";
+api.defaults.headers.common["WTS-LicenceType"] = "omni";
 api.defaults.headers.common["Content-Type"] = "application/json";
 api.defaults.auth = {
     password: "79Kkmw2",
